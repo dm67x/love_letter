9b9ee256d6bfbadd0e46194b51df84e4ab6b145d
KIR MACKA <macka_kir@hotmail.com>
2018-03-16 10:50:15 +0100

    class player done, missing one method showHand

--- core/player.h
+++ core/player.h
@@ -1,11 +1,44 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include "card.h"
+#include <list>
+
 
 class Player
 {
+
+private:
+    std::string name;
+    std::list<Card> cards;
+    int points                  = 0;
+    bool isProtected            = false;
+    bool isDead                 = false;
+
+
 public:
-    Player();
+    Player(std::string name);
+    std::string getName();
+    std::list<Card> getCards();
+
+    int getPoints();
+
+    bool isDead();
+    bool hasCard(Card * c);
+
+    void setDead();
+    void addPoint();
+    void addCard(Card c);
+    void switchHand(Player * p);
+    void pickCard();
+    void setProtection(bool value);
+    void discard();
+    void throwCard(Card * c);
+
+    Card * getCard();
+    Card * showHand(Player * p);
+
+
 };
 
 #endif // PLAYER_H
