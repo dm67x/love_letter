9e74bc0dcaf5eda5b43a2d30c206b6cf41bb0be2
Marija Kirandjiska <macka_kir@hotmail.com>
2018-03-17 21:50:49 +0100

    player.cpp bugs fixed, one error left

--- core/player.cpp
+++ core/player.cpp
@@ -1,39 +1,40 @@
 #include "player.h"
 
-
-Player::Player(std::string name)
-      : name(name)
-{
+Player::Player(string name){
+        this->name = name;
+        dead = false;
+        points = 0;
+        isProtected = false;
 }
 
 string Player::getName(){
     return name;
 }
 
-list<Card *> getCards(){
+list<Card *> Player::getCards(){
     return cards;
 }
 
-int getPoints(){
+int Player::getPoints(){
     return points;
 }
 
-bool isDead(){
-    return dead;
+bool Player::isDead(){
+    return this->dead;
 }
 
-bool hasProtection(){
+bool Player::hasProtection(){
     return isProtected;
 }
 
 //checks if player has card c in hand
-bool hasCard(Card &c){
+bool Player::hasCard(Card &c){
     bool res;
     if (cards.empty()){
         res=false;
     }else{
-        if(cards.front.isTheSameCardAs(c)  ||
-           cards.back.isTheSameCardAs(c)){
+        if(cards.front()->isTheSameCardAs(c)  ||
+           cards.back()->isTheSameCardAs(c)){
                res=true;
         }
     }
@@ -41,26 +42,26 @@ bool hasCard(Card &c){
 }
 
 
-void setDead(bool value){
+void Player::setDead(bool value){
     dead=value;
 }
 
-void addPoint(){
+void Player::addPoint(){
     points++;
 }
 
 //add the newly picked card last in the list
-void pickCard(Card &c){
-    cards.push_back(c);
+void Player::pickCard(Card &c){
+    cards.push_back(&c);
 }
 
 //switch cards with another player p
-void switchHand(Player &p){
-    cards.swap (p->getCards());
+void Player::switchHand(Player &p){
+    cards.swap(p.getCards());
 }
 
 
-void setProtection(bool value){
+void Player::setProtection(bool value){
     isProtected=value;
 }
 
@@ -68,24 +69,25 @@ void setProtection(bool value){
 //discard the only card the plays has in their hands
 //normally used to implement the effect
 //of the Prince just before picking a new card
-void discard(){
+void Player::discard(){
     cards.pop_front();
 }
 
 //play a chosen card
 //precond: c is in the list of cards and is chosen by the player
 //(determined through the GUI)
-void play(Card &c){
-    if(cards.front.isTheSameCardAs(c))
-        cards->pop_front();
-    else cards->pop_back();
+void Player::play(Card &c){
+    if(cards.front()->isTheSameCardAs(c))
+        cards.pop_front();
+    else cards.pop_back();
 }
 
 //returns the current and only card that the player has in his hand
 //can be used to implement the effect of the Priest
 //the player p to whom we show cards we're gonna get him through the GUI
-Card * showHand(){
-    return cards->front();
+Card * Player::showHand(){
+    return cards.front();
 }
+
 
 
