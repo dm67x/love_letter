a16308f8fdb51d8e73b1b86807943ca30ff2f3d5
dm67x <dm67x@outlook.fr>
2018-03-28 16:02:50 +0200

    merge with ihm branch

--- Utils.cpp
+++
@@ -1,7 +0,0 @@
-#include "Utils.h"
-
-bool Utils::mouseInside(sf::FloatRect rect, sf::Vector2i mousePosition)
-{
-    return rect.top <= mousePosition.y && rect.left <= mousePosition.x &&
-            rect.height >= mousePosition.y && rect.width >= mousePosition.x;
-}
